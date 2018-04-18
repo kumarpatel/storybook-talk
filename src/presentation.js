e09643ab3f24ce84e6cf4f8c5476aa36341db146
Kumar Patel <kumarpatel@getchalk.com>
2018-04-18 18:25:46 -0300

    More slides

--- src/presentation.js
+++ src/presentation.js
@@ -18,15 +18,18 @@ import {
   Fill,
   Appear,
   Layout,
-  Code
+  Code,
+  ComponentPlayground
 } from "spectacle";
 import CodeSlide from "spectacle-code-slide";
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
-import code from "./code.example";
-import storybookLogo from "./storybook-logo.svg";
-import slide1Gif from "./slide1.gif";
+import code from "./assets/code.example";
+import buttonComponent from "./assets/button-component.example";
+import storybookLogo from "./assets/storybook-logo.svg";
+import slide1Gif from "./assets/slide1.gif";
+import styleGuidist from "./assets/style-guidist.gif";
 
 // Require CSS
 require("normalize.css");
@@ -57,10 +60,10 @@ export default class Presentation extends React.Component {
           </Heading>
           <Notes>
             <h4>Question: show of hands</h4>
-            <ol>
+            <ul>
               <li>How many people have heard of Storybook?</li>
               <li>Familiarity with React?</li>
-            </ol>
+            </ul>
           </Notes>
         </Slide>
 
@@ -69,7 +72,6 @@ export default class Presentation extends React.Component {
             Goal:
           </Heading>
           <Appear
-            order={1}
             startValue={{ opacity: 0.1 }}
             endValue={{ opacity: 1 }}
             transitionDuration={1000}
@@ -86,13 +88,61 @@ export default class Presentation extends React.Component {
 
           <Notes>
             <li>Here are some of things I'll be covering during this talk.</li>
-            <ol>
-              <li>This talk is not to sell you on Storybook.</li>
+            <ul>
+              <li>This talk is not to sell you on Storybook</li>
+              <li>It is to show you how easy it is to integrate Storybook.</li>
               <li>
-                It is to sell you on the idea of investing in tooling similar to
-                this.
+                ...and hopefully help you make an informed decision over it's
+                alternatives
               </li>
-            </ol>
+            </ul>
+          </Notes>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary">
+          <Heading size={6} textColor="primary">
+            What is Storybook?
+          </Heading>
+          <Image src={slide1Gif} />
+
+          <Notes>
+            <li>Interactive styleguide</li>
+            <li>What is a styleguide?!?!</li>
+            <li>It's a way to easily understand your apps building blocks</li>
+            <li>
+              For example: A HeaderComponent, FooterComponent, CardComponents,
+              Button, etc
+            </li>
+            <li>
+              In this example, you see a Button component being represented in
+              different scenarios
+            </li>
+            <li>One with text</li>
+            <li>The other with emojis</li>
+          </Notes>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary">
+
+          <Heading size={6} textColor="primary">
+            Very similar to React Styleguidist
+          </Heading>
+          <Image src={styleGuidist} />
+
+          <Notes>
+            <li>Interactive styleguide</li>
+            <li>What is a styleguide?!?!</li>
+            <li>It's a way to easily understand your apps building blocks</li>
+            <li>
+              For example: A HeaderComponent, FooterComponent, CardComponents,
+              Button, etc
+            </li>
+            <li>
+              In this example, you see a Button component being represented in
+              different scenarios
+            </li>
+            <li>One with text</li>
+            <li>The other with emojis</li>
           </Notes>
         </Slide>
 
@@ -102,41 +152,29 @@ export default class Presentation extends React.Component {
           transition={["fade"]}
           lang="jsx"
           textSize=".6em"
-          code={code}
+          code={buttonComponent}
           ranges={[
-            { loc: [0, 1], title: "Importing React" },
-            { loc: [2, 30] }, //TodoList Component
-            { loc: [4, 12], title: "Contructor Method" },
-            { loc: [13, 25], title: "Render Method" },
-            { loc: [14, 17] }, //Creating TodoItem by mapping through the list
-            { loc: [18, 24] },
-            { loc: [26, 29], title: "addEvent Method" },
-            { loc: [31, 36], title: "TodoItem Component" },
-            { loc: [37, 72] }, //NewTodoItem Component
-            { loc: [39, 47] }, //Contructor Method
-            { loc: [48, 51] }, //componentDidMount
-            { loc: [52, 63] }, //render
-            { loc: [64, 69] }, //onChange
-            { loc: [70, 75] }, //onSubmit
-            { loc: [77, 78], title: "Rendering component in the DOM" }
+            { loc: [0, 16], title: "React Component Example" },
+            { loc: [6, 16], title: "Button component" },
+            { loc: [0, 6], title: "Styles used by this Button" },
+            { loc: [16, 21], title: "Can be used anywhere" }
           ]}
         />
-        <CodeSlide
-          bgColor="secondary"
-          textColor="primary"
-          transition={["fade"]}
-          lang="js"
-          code={code}
-          ranges={[
-            { loc: [0, 18], title: "Walking through some code" },
-            { loc: [0, 1], title: "The Beginning" },
-            { loc: [1, 2] },
-            { loc: [1, 2], note: "Heres a note!" },
-            { loc: [2, 3] },
-            { loc: [4, 7] },
-            { loc: [8, 10] }
-          ]}
-        />
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Layout>
+            <Fill>
+              <Appear>
+                <ComponentPlayground
+                  theme="external"
+                  previewBackgroundColor="#343434"
+                  code={buttonComponent}
+                />
+              </Appear>
+            </Fill>
+          </Layout>
+        </Slide>
+
         <Slide transition={["fade"]} bgColor="tertiary">
           <Layout>
             <Fill>
@@ -173,12 +211,6 @@ export default class Presentation extends React.Component {
               </Appear>
             </Fill>
           </Layout>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="secondary">
-          <Heading size={6} textColor="primary" caps>
-            <Image src={storybookLogo} style={{ height: 150, width: 350 }} />
-          </Heading>
-          <Image src={slide1Gif} />
         </Slide>
 
         <Slide transition={["fade"]} bgColor="tertiary">
